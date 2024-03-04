3d506fb27e4d5967fd9affad9cc1a3cf677f883f
Diyorbek Juraev <85333075+juraevdiyorbek@users.noreply.github.com>
2024-03-04 15:34:52 +0500

    Update lists.py
    comments add

--- lists.py
+++ lists.py
@@ -1,10 +1,13 @@
 def get_list(lst):
+    # avval stringga ozgartirdim listni ichidagilarni
     new_lst = list((map(lambda x : str(x),lst)))
+    # keyin uzunlik boyicha sortladim
     new_lst.sort(key=lambda x : len(x))
+    # ohirida yana int ga ogirib qoydim
     lst = list((map(lambda x : int(x),new_lst)))
     
     return lst
-
+# funksiyani chaqirib uni ishlatdim
 my_lst = list()
 num = int(input("Enter the number : "))
 for i in range(num):
